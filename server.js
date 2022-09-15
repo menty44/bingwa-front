26fb04b0d0c553e3c37d8a3e0e9b5ab5bdf42f68
Fredrick Oluoch <menty44@gmail.com>
2022-09-15 23:17:45 +0300

    Server file to render app

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const nStatic = require('node-static');
+
+const fileServer = new nStatic.Server('./public');
+
+http.createServer(function (req, res) {
+
+    fileServer.serve(req, res);
+
+}).listen(5000);
